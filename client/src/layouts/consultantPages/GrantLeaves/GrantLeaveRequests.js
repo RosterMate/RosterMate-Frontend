7b1912eb207fb58b2164c2bf98ea4c17945be46a
Thejan Weerasekara <thejanbweerasekara@gmail.com>
2023-10-17 15:33:53 +0530

    Updated leave requests page

--- client/src/layouts/consultantPages/GrantLeaves/GrantLeaveRequests.js
+++ client/src/layouts/consultantPages/GrantLeaves/GrantLeaveRequests.js
@@ -8,9 +8,15 @@ import Loading from "../../../components/Loading";
 import "./GrantLeaveRequests.css";
 import { USER_TYPE, USER_EMAIL } from "layouts/authentication/sign-in";
 
+// MUI components
+import MDTypography from "components/MDTypography";
+
 function GrantLeaveRequests() {
   const [leaveRequestsDetails, setleaveRequestsDetails] = useState([]);
   const [isLoadingLeaveReqDetails, setisLoadingLeaveReqDetails] = useState(true);
+
+  const [HistoryDetails, setHistoryDetails] = useState([]);
+  const [isLoadingHistoryDetails, setIsLoadingHistoryDetails] = useState(true);
 
   const data = {
     email: USER_EMAIL,
@@ -20,8 +26,10 @@ function GrantLeaveRequests() {
   useEffect(() => {
     Axios.post(`${BASE_URL}mainApp/leaveReqDetails`, data)
       .then((response) => {
-        setleaveRequestsDetails(response.data);
+        setleaveRequestsDetails(response.data["reqDetails"]);
+        setHistoryDetails(response.data["historyDetails"]);
         setisLoadingLeaveReqDetails(false);
+        setIsLoadingHistoryDetails(false);
       })
       .catch((error) => {
         console.error("Error fetching ward details:", error);
@@ -31,6 +39,9 @@ function GrantLeaveRequests() {
 
   return (
     <>
+      <MDTypography variant="h3" display="flex">
+        Leave Requests
+      </MDTypography>
       {isLoadingLeaveReqDetails ? (
         <Loading />
       ) : (
@@ -38,6 +49,33 @@ function GrantLeaveRequests() {
           <MDBox pt={3} mb={2}>
             <Grid container spacing={3}>
               {leaveRequestsDetails.map((req, id) => (
+                <Grid item xs={12} md={6} lg={3} key={id}>
+                  <MDBox mt={3}>
+                    <LeaveRequestCard
+                      Status={req.Status}
+                      Name={req.Name}
+                      Date={req.Date}
+                      FromTime={req.FromTime}
+                      ToTime={req.ToTime}
+                      Reason={req.Reason}
+                    />
+                  </MDBox>
+                </Grid>
+              ))}
+            </Grid>
+          </MDBox>
+        </div>
+      )}
+      <MDTypography variant="h3" display="flex">
+        History
+      </MDTypography>
+      {isLoadingHistoryDetails ? (
+        <Loading />
+      ) : (
+        <div className="">
+          <MDBox pt={3} mb={2}>
+            <Grid container spacing={3}>
+              {HistoryDetails.map((req, id) => (
                 <Grid item xs={12} md={6} lg={3} key={id}>
                   <MDBox mt={3}>
                     <LeaveRequestCard
