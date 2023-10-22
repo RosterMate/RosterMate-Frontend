409043ba461d876f093c24a3bbcd675d3684d8ff
Thejan Weerasekara <thejanbweerasekara@gmail.com>
2023-10-22 17:43:55 +0530

    shift change req page updated

--- client/src/layouts/doctorPages/ShiftChangeRequest/ShiftChangeRequestForm/index.js
+++ client/src/layouts/doctorPages/ShiftChangeRequest/ShiftChangeRequestForm/index.js
@@ -1,32 +1,65 @@
 import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import { USER_TYPE, USER_EMAIL } from "layouts/authentication/sign-in";
 
 import "./shiftchange.css";
 import MDTypography from "components/MDTypography";
-
-const initialValues = {
-  fromDate: new Date(),
-  toDate: new Date(),
-  fromShiftSlot: "",
-  toShiftSlot: "",
-  Reason: "",
-  Status: "",
-};
+import BASE_URL from "config/baseUrl";
+import MDButton from "components/MDButton";
+import Typography from "@mui/material/Typography";
+import PopupModal from "components/PopupModal";
 
 function ShiftChangeForm() {
-  const [form, setForm] = useState({
-    morningSeniorRegistrars: "",
-  });
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+  const [shiftsList, setShiftsList] = useState([]);
+
+  const [fromDate, setFromDate] = useState("");
+  const [fromShift, setFromShift] = useState("");
+  const [toDate, setToDate] = useState("");
+  const [toShift, setToShift] = useState("");
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    Axios.post(`${BASE_URL}mainApp/getShiftOptions`, { email: USER_EMAIL, type: USER_TYPE })
+      .then((response1) => {
+        setShiftsList(response1.data["shifts"]);
+        //console.log("shiftsList : ", response1.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching shift names:", error);
+      });
+  }, []);
+
+  const shiftOptions = shiftsList
+    ? [
+        <option key="default" value="" disabled>
+          Select a shift
+        </option>,
+        ...shiftsList.map((shift, index) => (
+          <option key={index} value={shift}>
+            {shift}
+          </option>
+        )),
+      ]
+    : [];
 
   const handleSubmitButtonClick = () => {
-    console.log(form);
+    if (fromDate == "" || toDate == "" || fromShift == "" || toShift == "") {
+      setErrorMessage("*Please fill all the fields");
+    } else {
+      setErrorMessage("");
+      setOpenModal(true);
+      setFromDate("");
+      setToDate("");
+      setFromShift("");
+      setToShift("");
+    }
   };
-
-  const [date, setDate] = useState(new Date());
-  const [fromDate, setFromDate] = useState(new Date());
-  const [toDate, setToDate] = useState(new Date());
-  const [fromShift, setFromShift] = useState();
-  const [toShift, setToShift] = useState();
-  const [selectsDoc, setSelectsDoc] = useState();
 
   return (
     <div className="accountsettings">
@@ -45,25 +78,23 @@ function ShiftChangeForm() {
           <label htmlFor="name">
             {" "}
             <MDTypography variant="h6" fontWeight="medium" textTransform="capitalize">
-              To Date
+              From Shift Slot
             </MDTypography>{" "}
           </label>
-          <input type="date" value={toDate} onChange={(e) => setToDate(e.target.value)} />
+
+          <select value={fromShift} onChange={(e) => setFromShift(e.target.value)}>
+            {shiftOptions}
+          </select>
         </div>
 
         <div className="form-group">
           <label htmlFor="name">
             {" "}
             <MDTypography variant="h6" fontWeight="medium" textTransform="capitalize">
-              From Shift Slot
+              To Date
             </MDTypography>{" "}
           </label>
-
-          <select value={fromShift} onChange={(e) => setFromShift(e.target.value)}>
-            <option value="1">Morning (08.00-13.00) </option>
-            <option value="2">Evening (13.00-17.00)</option>
-            <option value="3">Night (17.00-08.00)</option>
-          </select>
+          <input type="date" value={toDate} onChange={(e) => setToDate(e.target.value)} />
         </div>
 
         <div className="form-group">
@@ -75,42 +106,29 @@ function ShiftChangeForm() {
           </label>
 
           <select value={toShift} onChange={(e) => setToShift(e.target.value)}>
-            <option value="1">Morning (08.00-13.00) </option>
-            <option value="2">Evening (13.00-17.00)</option>
-            <option value="3">Night (17.00-08.00)</option>
+            {shiftOptions}
           </select>
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="name">
-            {" "}
-            <MDTypography variant="h6" fontWeight="medium" textTransform="capitalize">
-              Select Doctor
-            </MDTypography>{" "}
-          </label>
-
-          <select value={selectsDoc} onChange={(e) => setSelectsDoc(e.target.value)}>
-            <option value="1">Doctor 1</option>
-            <option value="2">Doctor 2</option>
-            <option value="3">Doctor 3</option>
-            <option value="4">Doctor 4</option>
-          </select>
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="name">
-            {" "}
-            <MDTypography variant="h6" fontWeight="medium" textTransform="capitalize">
-              Submit the form here
-            </MDTypography>{" "}
-          </label>
-          <button className="mainbutton1" onClick={handleSubmitButtonClick}>
-            <MDTypography variant="h6" fontWeight="medium">
-              Submit
-            </MDTypography>
-          </button>
         </div>
       </div>
+      {errorMessage && (
+        <Typography
+          variant="body2"
+          color="error"
+          style={{ alignSelf: "flex-start", alignSelf: "center", justifySelf: "center" }}
+        >
+          {errorMessage}
+        </Typography>
+      )}
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <MDButton color="info" onClick={handleSubmitButtonClick}>
+          Submit
+        </MDButton>
+      </div>
+      <PopupModal
+        open={openModal}
+        message="Shift change request sent successfully"
+        onClose={handleCloseModal}
+      />
     </div>
   );
 }
